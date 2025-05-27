01043202072e514697c5490404954f1f2c1ea5b2
Ubuntu <ubuntu@ip-172-31-25-134.eu-west-1.compute.internal>
2025-05-27 08:24:07 +0000

    loops file

--- first.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "md mohi uddin"
-echo "Apel Mahmud"
-echo "zahir Uddin"
-echo "Md Mohi Uddin"
-mkdir -p Mohi
-echo "ye leh, bana diya"
-echo "tune too beizaati kar di"
-echo "abbah nahi manenge" > farhan.txt
